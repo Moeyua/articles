775ccbbdb09a381e8e95587bfb0dab0e73bae360
moeyua <moeyua@foxmail.com>
2020-09-30 16:42:12 +0800

    页面样式

--- src/components/contents.vue
+++ src/components/contents.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-      <div v-for="item in list.items" :key="item.name">
-          <a :href="src(list.dir,item.name)">{{item.name}}</a>
-      </div>
+    <a :href="src(list.dir,item.name)" class="articles" v-for="item in list.items" :key="item.name">
+      {{item.name}}
+    </a>
   </div>
 </template>
 
@@ -25,5 +25,16 @@ export default {
 </script>
 
 <style scoped>
-
+.articles{
+  display: block;
+  height: 100px;
+  width: calc(100% - 20px);
+  background: #ebebeb;
+  margin: 10px 10px;
+  font-size: 20px;
+  font-weight: 700;
+  text-decoration: none;
+  color: black;
+  line-height: 100px;
+}
 </style>
